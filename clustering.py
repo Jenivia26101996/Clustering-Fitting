0fc8b15588c6ba98d26998d54920c19d433822d3
Jenivia26101996 <jenivia26@gmail.com>
2023-01-19 11:28:43 +0000

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,73 +1,129 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 18 01:29:26 2023
-
-@author: Huawei
-"""
 import pandas as pd
-import wbgapi as wb
-import sklearn
-import seaborn as sns
-from sklearn.datasets import make_blobs
-from numpy import array, exp
-import itertools as iter
-from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-import sklearn.cluster as cluster
+import numpy as np
+import matplotlib as mpl
+import seaborn as sns
+from sklearn import cluster
+from sklearn import preprocessing
+import plotly.express as px
 import sklearn.metrics as skmet
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+"""
+Created a function electric to manipulate the data using pandas dataframes which takes a csv file as argument, 
+reads a dataframe in Worldbank format which is electric consumption and returns two dataframes: 
+one with years as columns and one with countries as columns.
+iloc:for selective columns in dataframe
+fillna(0):will replace Nan values with 0
+Transposed(T): is used to return transposed columns
+kind='line': line plot function in pandas
+bbox_to_anchor: to place leegend outside the box
+label:display labels
+savefig:saves image in the directory
+linestyle: we can display different types of line
+"""
 
-def worldbank(filename):
-    df=pd.read_csv(filename,skiprows=(4))
-    df_transpose=df.transpose()
-    return df,df_transpose
-a,b=worldbank("C:/Users/Huawei/Desktop/ADSAssign2/Clustering-Fitting/API_19_DS2_en_csv_v2_4773766.csv")
+def data(filename):
+    df_electric=pd.read_csv(filename)
+    df_country=df_electric['Country Name']
+    df_electric=df_electric.iloc[:,[59,125,191,257]]
+    df_electric = df_electric.fillna(0)
+    print(df_electric)
+    df_electric.insert(loc=0,column='Country Name',value=df_country)
+    df_electric=df_electric.dropna(axis=1)
+    df_electrict=df_electric.set_index('Country Name').T
+    #print(df)
+    return df_electric,df_electrict 
+a,b=data("merge_df.csv")
 print(a)
 print(b)
+def norm(array):
+    """Returns array normalised to [0,1]. Array can be a numpy array or a column of a dataframe"""
+    min_val = np.min(array)
+    max_val = np.max(array)
+    scaled = (array-min_val) / (max_val-min_val)
+    return scaled
+  
+def norm_df(df, first=0, last=None):
+    """Returns all columns of the dataframe normalised to [0,1] with the
+exception of the first (containing the names)
+Calls function norm to do the normalisation of one column, but
+doing all in one function is also fine.
+First, last: columns from first to last (including) are normalised.
+Defaulted to all. None is the empty entry. The default corresponds
+"""
+# iterate over all numerical columns
+    for col in df.columns[first:last]: # excluding the first column
+     df[col] = norm(df[col])
+    return df
 
-ecn_indc = ['NE.DAB.TOTL.ZS','NY.GDP.MKTP.CD']
-cod_contry = ["BMU","CHE",'DNK','BGR','BGD','ARG','GBR','IND','BRA','JAM']
-cli_indc=['EG.ELC.RNWX.KH','EN.ATM.CO2E.GF.KT']
-ecn_data  = wb.data.DataFrame(ecn_indc, cod_contry, mrv=7)
-cli_data  = wb.data.DataFrame(cli_indc, cod_contry, mrv=7)
-#NE.DAB.TOTL.ZS: Total expenditure
-#NY.GDP.MKTP.CD: USD GDP of a country
-#EG.ELC.RNWX.KH:	Electricity production from renewable sources %
-#EN.ATM.CO2E.GF.KT: Emissions of Carbon dioxide from fuel 
-# ECNMY INDICATOR
-ecn_data.columns = [b.replace('YR','') for b in ecn_data.columns]      
-ecn_data=ecn_data.stack().unstack(level=1)                             
-ecn_data.index.names = ['Ctry_Code', 'Year']                           
-ecn_data.columns                                                     
-ecn_data.fillna(0)
-print(ecn_data.head(9))
 
-# CLMATE INDICATOR
-cli_data.columns = [c.replace('YR','') for c in cli_data.columns]      
-cli_data=cli_data.stack().unstack(level=1)                             
-cli_data.index.names = ['Ctry_Code', 'Year']                           
-cli_data.columns                                                     
-cli_data.fillna(0)
-cli_data.head(9)
+# heatmap
+plt.figure(figsize = (10,8))
+sns.heatmap(a.corr(), cmap = 'Wistia', annot = True)
+plt.title('Heatmap for the Data')
+plt.show()
 
-#Preprtion of the data
-dfrm1=ecn_data.reset_index()
-dfrm3=dfrm1.fillna(0)
-dfrm2=cli_data.reset_index()
-dfrm4=dfrm2.fillna(0)
+plt.figure(figsize = (10,8),dpi=144)
+pd.plotting.scatter_matrix(a, figsize=(9.0, 9.0))
+plt.tight_layout() # helps to avoid overlap of labels
+plt.show()
 
-#Getting the indicators for all the countries
-dfrm = pd.merge(dfrm3, dfrm4)
-X=dfrm.head(10)
 
-print(X)
 
-#Normalization of the dfrm values
-df1 = dfrm.iloc[:,2:]
-dfrm.iloc[:,2:] = (df1-df1.min())/ (df1.max() - df1.min())
-dfrm.head(7)
+# extract columns for fitting
+df_fit = a[['Tuberculosis Prevalance data in 2015','Death rate data in 2015']].copy()
+# normalise dataframe and inspect result
+# normalisation is done only on the extract columns. .copy() prevents
+# changes in df_fit to affect df_fish. This make the plots with the
+# original measurements
+df_fit = norm_df(df_fit)
+print(df_fit.describe())
+print()
 
-#Clustering the value of total expenditure for different countries
-plt.scatter(data=dfrm, x="Ctry_Code", y="NE.DAB.TOTL.ZS",cmap="cool")
-plt.legend(loc='lower right')
+for ic in range(2, 7):
+# set up kmeans and fit
+    kmeans = cluster.KMeans(n_clusters=ic)
+    kmeans.fit(df_fit)
+# extract labels and calculate silhoutte score
+labels = kmeans.labels_
+print (ic, skmet.silhouette_score(df_fit, labels))
+# Plot for four clusters
+kmeans = cluster.KMeans(n_clusters=4)
+kmeans.fit(df_fit)
+# extract labels and cluster centres
+labels = kmeans.labels_
+cen = kmeans.cluster_centers_
+plt.figure(figsize=(6.0, 6.0))
+# Individual colours can be assigned to symbols. The label l is used to the␣
+# l-th number from the colour table.
+plt.scatter(df_fit["Tuberculosis Prevalance data in 2015"], df_fit["Death rate data in 2015"], c=labels, cmap="Accent",s = 50 )
+# colour map Accent selected to increase contrast between colours
+# show cluster centres
+for ic in range(4):
+    xc, yc = cen[ic,:]
+plt.plot(xc, yc, "dk", markersize=10)
+plt.xlabel("Tuberculosis Prevalance data in 2015")
+plt.ylabel("Death rate data in 2015")
+plt.title("4 clusters")
+plt.show()
+# #-----------------------
+# # Plot for five clusters
+kmeans = cluster.KMeans(n_clusters=3)
+kmeans.fit(df_fit)
+# extract labels and cluster centres
+labels = kmeans.labels_
+cen = kmeans.cluster_centers_
+plt.figure(figsize=(6.0, 6.0))
+# Individual colours can be assigned to symbols. The label l is used to the␣
+# l-th number from the colour table.
+plt.scatter(df_fit["Tuberculosis Prevalance data in 2015"], df_fit["Death rate data in 2015"], c=labels, cmap="Accent")
+# colour map Accent selected to increase contrast between colours
+# show cluster centres
+for ic in range(3):
+    xc, yc = cen[ic,:]
+plt.plot(xc, yc, "dk", markersize=10)
+plt.xlabel("Tuberculosis Prevalance data in 2015")
+plt.ylabel("Death rate data in 2015")
+plt.title("3 clusters")
 plt.show()
